56d2805f197132cbeddc0ad3fdef989a69de0c94
Isaac Ruiz <isaac.rga@gmail.com>
2018-02-07 22:54:42 -0600

    Fixed mailing

--- sendcontact.php
+++ sendcontact.php
@@ -1,31 +1,60 @@
 <?php
 
 // Define some constants
-define( "RECIPIENT_NAME", "Homero Ruiz" ); //UPDATE THIS TO YOUR NAME
-define( "RECIPIENT_EMAIL", "isaac.rga@gmail.com" ); //UPDATE THIS TO YOUR EMAIL ID
+define( "RECIPIENT_EMAIL", "hmro.contact@gmail.com" ); //UPDATE THIS TO YOUR EMAIL ID
 define( "EMAIL_SUBJECT", "Website Visitor Message" ); //UPDATE THIS TO YOUR SUBJECT
 
 // Read the form values
-$success = false;
 $senderName = isset( $_POST['name'] ) ? preg_replace( "/[^\.\-\' a-zA-Z0-9]/", "", $_POST['name'] ) : "";
 $senderEmail = isset( $_POST['email'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['email'] ) : "";
 $original_message = isset( $_POST['message'] ) ? preg_replace( "/(From:|To:|BCC:|CC:|Subject:|Content-Type:)/", "", $_POST['message'] ) : "";
 $message = 'Name: '.$senderName.'<br/>Email: '.$senderEmail.'<br/>Message: '.$original_message;
 
-// If all values exist, send the email
-if ( $senderName && $senderEmail && $message ) {
-  $recipient = RECIPIENT_NAME . " <" . RECIPIENT_EMAIL . ">";
-  $headers = "From: " . $senderName . " <" . $senderEmail . ">\n";
-  $headers .= "MIME-Version: 1.0\n";
-  $headers .= "Content-Type: text/HTML; charset=ISO-8859-1\n";
-  $success = mail( $recipient, EMAIL_SUBJECT, $message, $headers );
-}
+$url = 'https://api.sendgrid.com/';
+$user = 'app87474532@heroku.com';
+$pass = 'wwnmvbls1896';
 
-if ( $success )
+$params = array(
+    'api_user'  => $user,
+    'api_key'   => $pass,
+    'to'        => RECIPIENT_EMAIL,
+    'subject'   => EMAIL_SUBJECT,
+    'html'      => $message,
+    'from'      => $senderName
+  );
+
+$request =  $url.'api/mail.send.json';
+
+// Generate curl request
+$session = curl_init($request);
+// Tell curl to use HTTP POST
+curl_setopt ($session, CURLOPT_POST, true);
+// Tell curl that this is the body of the POST
+curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
+// Tell curl not to return headers, but do return the response
+curl_setopt($session, CURLOPT_HEADER, false);
+// Tell PHP not to use SSLv3 (instead opting for TLS)
+//curl_setopt($session, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
+
+//Turn off SSL
+curl_setopt($session, CURLOPT_SSL_VERIFYPEER, false);//New line
+curl_setopt($session, CURLOPT_SSL_VERIFYHOST, false);//New line
+
+curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+
+// obtain response
+$response = curl_exec($session);
+
+// print everything out
+var_dump($response,curl_error($session),curl_getinfo($session));
+
+curl_close($session);
+
+if ( $response != false )
 {
 ?>
 	<script>
-		window.location='thanks.html';
+		window.location='thanks.php';
 	</script>
 <?php
 }
@@ -34,5 +63,3 @@ else
 	echo "<h1>ooops, something went wrong. Please try again.</h1>";
 }
 ?>
-
-
---
+++ thanks.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <!-- Page Info / Información de Página -->
+    <meta charset="utf-8">
+    <title>HMRO© - Official Website</title>
+    <meta name="Research, Art and Design" content="HMRO© - Official Portfolio Website">
+    <meta name="Homero Ruiz García" content="Portfolio Website">
+
+    <!-- Mobile Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" href="images/faviconhmro.png"/>
+
+    <!-- Fonts -->
+    <link rel="stylesheet" href="fonts/webfonts.css">
+    <link href='//fonts.googleapis.com/css?family=Raleway:400,300,600%7CLato:400,300,100,700,900' rel='stylesheet' type='text/css'>
+
+    <!-- Icon Fonts -->
+    <link rel="stylesheet" href="stylesheets/ionicons.min.css">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="stylesheets/bootstrap.css">
+    <link rel="stylesheet" href="elements/css/elements.css">
+    <link rel="stylesheet" href="stylesheets/sinister.css">
+    <link rel="stylesheet" href="stylesheets/venobox.css">
+    <link rel="stylesheet" href="stylesheets/owl.carousel.css">
+    <link rel="stylesheet" href="stylesheets/slimmenu.css">
+    <link rel="stylesheet" href="stylesheets/main.css">
+    <link rel="stylesheet" href="stylesheets/main-bg.css">
+    <link rel="stylesheet" href="stylesheets/main-responsive.css">
+
+
+</head>
+
+<body>
+
+
+
+    <!-- Preloader -->
+    <?php include_once("partials/preloader.html"); ?>
+
+    <!-- Nav Link Show Panel -->
+    <div class="link-show-poster">
+      <div class="valign"><h1 class="black font2"></h1></div>
+    </div>
+
+
+    <!-- Header -->
+    <?php include_once("partials/menu.html"); ?>
+    <!-- end : masthead -->
+
+
+    <!-- Mobile Only Navigation / Navegacion para Mobile -->
+    <?php include_once("partials/mobile-menu.html"); ?>
+    <!-- end : mobile navigation -->
+
+
+    <!-- Master Content / Contenido Master -->
+    <section class="mastwrap add-bottom-half">
+      <section class="container about  halfheight add-bottom">
+      <div class="valign">
+        <div class="row">
+              <article class="col-md-12 text-center ">
+                <h6 class="promo-text  font1 grey">Thank You</h6><br/>
+                <h3 class="main-heading  font2 black">We got your message.<br/>We will reply very soon.</h3>
+              </article>
+        </div>
+      </div>
+      </section>
+    </section>
+    <!-- end : mastwrap -->
+
+
+    <!-- Footer -->
+    <footer class="mastfoot silver-bg ">
+
+      <div class="container">
+        <div class="row">
+          <article class="col-md-1">
+            <nav class="footnav ">
+                  <ul>
+                    <li><a class="font1" href="http://www.hmro.co/">Design</a></li>
+                    <li><a class="font1" href="#"></a></li>
+                    <li><a class="font1" href="#"></a></li>
+                  </ul>
+            </nav>
+          </article>
+          <article class="col-md-3 text-left">
+            <h3 class="font2 black">hmro.co</h3>
+            <h6 class="font1 grey">Based in Weimar Germany. Art, Technology &amp Design Research. Think, create, collaborate.</h6>
+          </article>
+          <article class="col-md-6 text-left">
+            <h3 class="font2 black">&copy;</h3>
+
+            <h6 class="font1 grey">Copyright 2018. Weimar, Germany<br/><a href="http://www.hmro.co/">Sitemap</a></h6>
+          </article>
+          <article class="col-md-2 text-right">
+            <img alt="" title="" class="img-responsive foot-logo" src="images/logo.png">
+          </article>
+        </div>
+      </div>
+
+    </footer>
+
+    <!-- end : mastfoot -->
+
+
+    <!-- End Layout
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+
+    <!-- JS
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <script src="javascripts/libs/common.js"></script>
+    <script src="elements/js/elements.js"></script>
+    <script src="javascripts/custom/main.js"></script>
+
+
+</body>
+
+</html>
