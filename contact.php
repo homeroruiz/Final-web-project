6c5480916781e899e95b876ab04c489323922da3
Isaac Ruiz <isaac.rga@gmail.com>
2018-02-07 23:58:30 -0600

    Removed map from contact page

--- contact.php
+++ contact.php
@@ -49,17 +49,17 @@
     </div>
 
 
-
     <!-- Header -->
     <?php include_once("partials/menu.html"); ?>
     <!-- end : masthead -->
 
+    <!-- Mobile Only Navigation / Navegacion para Mobile -->
+    <?php include_once("partials/mobile-menu.html"); ?>
+    <!-- end : mobile navigation -->
+
 
     <!-- Master Content / Contenido Master -->
     <section class="mastwrap">
-
-
-
       <section class="container about  add-bottom">
 
         <div class="row">
@@ -106,27 +106,12 @@
         </div>
 
       </section>
-
-
-
-
-      <div class="container-fluid">
-            <div class="row">
-                <article class="col-md-12 map-wrap no-pad">
-                    <div id="map" class=" fullheight"></div>
-                </article>
-            </div>
-      </div>
-
-
     </section>
 
 
+    <!-- Footer -->
 
-  <!-- Footer -->
-
-<footer class="mastfoot silver-bg ">
-
+    <footer class="mastfoot silver-bg ">
       <div class="container">
         <div class="row">
           <article class="col-md-1">
@@ -152,9 +137,7 @@
           </article>
         </div>
       </div>
-
     </footer>
-
 
     <!-- JS
     –––––––––––––––––––––––––––––––––––––––––––––––––– -->
@@ -162,44 +145,6 @@
     <script src="elements/js/elements.js"></script>
     <script src="javascripts/custom/form-validation.js"></script>
     <script src="javascripts/custom/main.js"></script>
-
-  <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>
-
-<script type="text/javascript">
-            // When the window has finished loading create our google map below
-            google.maps.event.addDomListener(window, 'load', init);
-
-            function init() {
-                // Basic options for a simple Google Map
-                // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-                var mapOptions = {
-                    // How zoomed in you want the map to start at (always required)
-                    zoom: 14,
-
-                    // The latitude and longitude to center the map (always required)
-                    center: new google.maps.LatLng(50.9745958, 11.329037299999982), // Weimar, Germany.
-
-                    // How you would like to style the map.
-                    // This is where you would paste any style found on Snazzy Maps.
-                    styles: [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":17}]}]
-                };
-
-                // Get the HTML DOM element that will contain your map
-                // We are using a div with id="map" seen below in the <body>
-                var mapElement = document.getElementById('map');
-
-                // Create the Google Map using our element and options defined above
-                var map = new google.maps.Map(mapElement, mapOptions);
-
-                // Let's also add a marker while we're at it
-                var marker = new google.maps.Marker({
-                    position: new google.maps.LatLng(50.9745958, 11.329037299999982),
-                    map: map,
-                    title: 'Snazzy!'
-                });
-            }
-        </script>
-
 </body>
 
 </html>
